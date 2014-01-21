18d4bf5bbfc2c96568ff65353199a3cb4423dc6b
blerer <b_lerer@hotmail.com>
2014-01-21 13:00:28 +0100

    Addition of new assert methods for Maps

--- src/main/java/io/horizondb/test/AssertCollections.java
+++ src/main/java/io/horizondb/test/AssertCollections.java
@@ -18,6 +18,7 @@ package io.horizondb.test;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -100,7 +101,44 @@ public final class AssertCollections {
                        collection.contains(expectedElements[i]));
         }
     }
+    
+    /**
+     * Verifies that the specified map contains only the specified key with the specified value.
+     * 
+     * @param <K> the key type
+     * @param <V> the value type
+     * @param map the map to verify
+     * @param key the expected key
+     * @param value the expected key value
+     */
+    public static <K, V> void assertMapContains(Map<K, V> map, K key, V value) {
+        
+        assertEquals("the size of the map does not match the expected one.", 1, map.size());
+        
+        assertTrue("the map " + map + " does not contains the key: " + key, map.containsKey(key));
+        assertEquals(map.get(key), value);
+    }
 
+    /**
+     * Verifies that the specified map contains only the specified key with the specified value.
+     * 
+     * @param <K> the key type
+     * @param <V> the value type
+     * @param map the map to verify
+     * @param key1 the expected first key
+     * @param value1 the expected first key value
+     */
+    public static <K, V> void assertMapContains(Map<K, V> map, K key1, V value1, K key2, V value2) {
+        
+        assertEquals("the size of the map does not match the expected one.", 2, map.size());
+        
+        assertTrue("the map " + map + " does not contains the key: " + key1, map.containsKey(key1));
+        assertEquals(map.get(key1), value1);
+        
+        assertTrue("the map " + map + " does not contains the key: " + key2, map.containsKey(key2));
+        assertEquals(map.get(key2), value2);
+    }
+    
     /**
      * Verifies that the specified array contains the specified elements in the specified order.
      * 
